48ce311e1bc49e5e4c7cae9932cd83f411e48da5
Rafael Mafra <69654497+rafaelmaframg@users.noreply.github.com>
2022-04-15 14:02:54 +0100

    finalizado exercicio final e adicionado link para retorno

--- exerciciofinal/jscript.js
+++ exerciciofinal/jscript.js
@@ -13,16 +13,23 @@ function finish(){
     var resultado = document.getElementById('result')
     var maior = 0
     var menor = 100
-    var media = 0
+    var soma = 0
+    console.log(typeof(lista_numeros[0]))
+    
     for(num in lista_numeros){
-
+        if(lista_numeros[num] > maior){
+            maior = lista_numeros[num]
+        }else if(lista_numeros[num] < menor){
+            menor = lista_numeros[num]
+        }
+        soma += lista_numeros[num]
     }
 
 
     resultado.className = 'bottomon'
-    resultado.innerHTML = `There area ${lista_numeros} registered numbers.<br>`
-    resultado.innerHTML += `The biggest number is ${lista_numeros}.<br>`
-    resultado.innerHTML += `The smallest number is ${lista_numeros}.<br>`
-    resultado.innerHTML += `The sum of all of them ${lista_numeros}<br>`
-    resultado.innerHTML += `The average of numbers is${lista_numeros}.`
+    resultado.innerHTML = `There area ${lista_numeros.length} registered numbers.<br>`
+    resultado.innerHTML += `The biggest number is ${maior}.<br>`
+    resultado.innerHTML += `The smallest number is ${menor}.<br>`
+    resultado.innerHTML += `The sum of all of them ${soma}<br>`
+    resultado.innerHTML += `The average of numbers is ${soma/lista_numeros.length}.`
 }
