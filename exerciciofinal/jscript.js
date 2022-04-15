256305cf049ab0a386b3257dcd3c49e53f938858
Rafael Mafra <69654497+rafaelmaframg@users.noreply.github.com>
2022-04-15 12:49:32 +0100

    adicionado estilo e animacao para div resultado

--- exerciciofinal/jscript.js
+++ exerciciofinal/jscript.js
@@ -11,9 +11,18 @@ function clickadd(){
 
 function finish(){
     var resultado = document.getElementById('result')
-    resultado.innerHTML = `Ao Todo temos ${lista_numeros} numeros cadastrados.<br>`
-    resultado.innerHTML += `O maior valor informado foi ${lista_numeros}.<br>`
-    resultado.innerHTML += `O menor valor informado foi ${lista_numeros}.<br>`
-    resultado.innerHTML += `Somando todos valores, temos ${lista_numeros}<br>`
-    resultado.innerHTML += `A Média dos valores digitados é ${lista_numeros}.`
+    var maior = 0
+    var menor = 100
+    var media = 0
+    for(num in lista_numeros){
+
+    }
+
+
+    resultado.className = 'bottomon'
+    resultado.innerHTML = `There area ${lista_numeros} registered numbers.<br>`
+    resultado.innerHTML += `The biggest number is ${lista_numeros}.<br>`
+    resultado.innerHTML += `The smallest number is ${lista_numeros}.<br>`
+    resultado.innerHTML += `The sum of all of them ${lista_numeros}<br>`
+    resultado.innerHTML += `The average of numbers is${lista_numeros}.`
 }
