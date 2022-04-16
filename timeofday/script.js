2ebcd01fb7b46ee4e797b18f4e9f329fae1c8c5d
Rafael Mafra <69654497+rafaelmaframg@users.noreply.github.com>
2022-04-16 11:30:35 +0100

    atualizado todos botoes de voltar para centralizar em uma unica fonte

--- timeofday/script.js
+++ timeofday/script.js
@@ -7,17 +7,17 @@ var footer = document.querySelector('footer')
 msg.innerText = `It's ${hora}:${data.getMinutes()} hrs`
 if (hora >= 0 && hora < 12){
     //bom dia
-    img.src = './media/img1.jpg'
+    img.src = '../media/img1.jpg'
     document.body.style.background = '#f5ffbf85'
     header.style.color = 'gray'
     footer.style.color = 'gray'
 } else if (hora >= 12 && hora < 18){
     //boa tarde
-    img.src = './media/img2.jpg'
+    img.src = '../media/img2.jpg'
     window.document.body.style.background = '#eaa570'
 } else {
     //boa noite
-    img.src = "./media/img3.jpg"
+    img.src = "../media/img3.jpg"
     document.body.style.background = '#325281e0'
 }
 
@@ -25,21 +25,21 @@ function change(img){
     if(img == 'dia'){
         var hora = 9
         var img = window.document.getElementById('imagem')
-        img.src = './media/img1.jpg'
+        img.src = '../media/img1.jpg'
         window.document.body.style.background = '#f5ffbf85'
         header.style.color = 'gray'
         footer.style.color = 'gray'
     }if(img == 'tarde'){
         var hora = 13
         var img = window.document.getElementById('imagem')
-        img.src = './media/img2.jpg'
+        img.src = '../media/img2.jpg'
         window.document.body.style.background = '#eaa570'
         header.style.color = 'white'
         footer.style.color = 'white'
     }if(img == 'noite'){
         var hora = 20
         var img = window.document.getElementById('imagem')
-        img.src = "./media/img3.jpg"
+        img.src = "../media/img3.jpg"
         window.document.body.style.background = '#325281e0'
         header.style.color = 'white'
         footer.style.color = 'white'
