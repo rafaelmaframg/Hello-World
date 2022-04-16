0027391aee50bd4fab769ce5877910a0b13d410f
Rafael Mafra <69654497+rafaelmaframg@users.noreply.github.com>
2022-04-16 11:16:40 +0100

    adicionado nova pagina e corrigido os links

--- tabuada/script.js
+++ tabuada/script.js
@@ -1,13 +1,12 @@
 function coleta(){
     var numero = Number(document.getElementById('entrada').value)
-    var resultado = document.querySelectorAll('option')
-    console.log(numero)
-    console.log(resultado)
+    var resultado = document.getElementById('resultado')
+    resultado.innerHTML = ''
     if(!numero){
         alert('Insira um numero valido!')
     }else{
         for(c=0;c<10;c++){
-            resultado[c].innerHTML +=  ` ${numero} x ${c} = ${numero*c}`
+            resultado.innerHTML +=  `<option> ${numero} x ${c} = ${numero*c}</option>`
         }
     }
 }
