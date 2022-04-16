b7181c83e5325cb8834324301656f921f6c6c6e6
Rafael Mafra <69654497+rafaelmaframg@users.noreply.github.com>
2022-04-16 10:36:54 +0100

    update js function, add icon to change timeofday

--- timeofday/script.js
+++ timeofday/script.js
@@ -18,24 +18,27 @@ if (hora >= 0 && hora < 12){
 } else {
     //boa noite
     img.src = "./media/img3.jpg"
-    document.body.style.background = '##325281e0'
+    document.body.style.background = '#325281e0'
 }
 
 function change(img){
     if(img == 'dia'){
         var hora = 9
+        var img = window.document.getElementById('imagem')
         img.src = './media/img1.jpg'
         window.document.body.style.background = '#f5ffbf85'
         header.style.color = 'gray'
         footer.style.color = 'gray'
     }if(img == 'tarde'){
         var hora = 13
+        var img = window.document.getElementById('imagem')
         img.src = './media/img2.jpg'
         window.document.body.style.background = '#eaa570'
         header.style.color = 'white'
         footer.style.color = 'white'
     }if(img == 'noite'){
         var hora = 20
+        var img = window.document.getElementById('imagem')
         img.src = "./media/img3.jpg"
         window.document.body.style.background = '#325281e0'
         header.style.color = 'white'
